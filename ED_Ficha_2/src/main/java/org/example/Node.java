dd1fae6dc49eb4e6623252705e93ead3190828f6
Guilherme Barreiro <80386801+Guilherme-Barreiro@users.noreply.github.com>
2025-10-01 17:36:17 +0100

    a

---
+++ ED_Ficha_2/src/main/java/org/example/Node.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Node <T>{
+    T data;
+    Node<T> next;
+    Node<T> prev;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public Node() {
+        this.data = null;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", next=" + next +
+                ", prev=" + prev +
+                '}';
+    }
+}
